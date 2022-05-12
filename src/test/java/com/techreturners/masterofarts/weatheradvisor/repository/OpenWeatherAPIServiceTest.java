d710e2e55c66faa2a91a533fad810ce5a8fce558
Matt DaSilva <matt_das@hotmail.co.uk>
2022-05-12 17:32:40 +0100

    Remove demonstration test code

--- src/test/java/com/techreturners/masterofarts/weatheradvisor/repository/OpenWeatherAPIServiceTest.java
+++ src/test/java/com/techreturners/masterofarts/weatheradvisor/repository/OpenWeatherAPIServiceTest.java
@@ -1,21 +1,5 @@
 package com.techreturners.masterofarts.weatheradvisor.repository;
 
-import com.techreturners.masterofarts.weatheradvisor.model.Weather;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class OpenWeatherAPIServiceTest {
-
-    @Autowired
-    private OpenWeatherAPIServiceImp openWeatherAPIServiceImp;
-
-    @Test
-    public void testGetWeather() {
-        Weather w = openWeatherAPIServiceImp.getWeather();
-        System.out.println(w);
-    }
-
-
+    
 }
