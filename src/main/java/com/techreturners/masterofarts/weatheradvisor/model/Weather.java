bb9c3bb60d2ed76ab9dfc2440058146e6f5a8495
Matt DaSilva <matt_das@hotmail.co.uk>
2022-05-12 14:11:04 +0100

    Create Weather model fields

--- src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Weather.java
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Weather.java
@@ -1,4 +1,14 @@
 package com.techreturners.masterofarts.weatheradvisor.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Weather {
+
+    double temp;
+    long rain;
+    int cloud;
+
 }
