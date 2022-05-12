2670c1f846eb9ec6c1884c2d5260dbff2fe475d3
Matt DaSilva <matt_das@hotmail.co.uk>
2022-05-12 13:26:53 +0100

    Initial commit

---
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/WeatherAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.techreturners.masterofarts.weatheradvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techreturners/masterofarts/weatheradvisor/WeatherAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.masterofarts.weatheradvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
