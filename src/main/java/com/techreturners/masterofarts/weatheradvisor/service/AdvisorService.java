83b5e3fd9605d859718999c990895128c05c7bde
Matt DaSilva <matt_das@hotmail.co.uk>
2022-05-12 14:16:17 +0100

    Implement AdvisorService getWeather

--- src/main/java/com/techreturners/masterofarts/weatheradvisor/service/AdvisorService.java
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/service/AdvisorService.java
@@ -1,10 +1,17 @@
 package com.techreturners.masterofarts.weatheradvisor.service;
 
 import com.techreturners.masterofarts.weatheradvisor.model.Weather;
+import com.techreturners.masterofarts.weatheradvisor.repository.ExternalWeatherAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdvisorService {
 
+    @Autowired
+    private ExternalWeatherAPIService externalWeatherAPIService;
+
     public Weather getWeather(){
-        return null;
+        return externalWeatherAPIService.getWeather();
     }
 }
