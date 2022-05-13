94eba0d96bfa5a18ab019ecb64e08da925d8714e
MarcusC67 <93521177+MarcusC67@users.noreply.github.com>
2022-05-13 10:41:35 +0100

    Initial commit - add first endpoint in Controller

--- src/main/java/com/techreturners/masterofarts/weatheradvisor/repository/OpenWeatherAPIServiceImp.java
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/repository/OpenWeatherAPIServiceImp.java
@@ -1,7 +1,9 @@
 package com.techreturners.masterofarts.weatheradvisor.repository;
 
 import com.techreturners.masterofarts.weatheradvisor.model.Weather;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OpenWeatherAPIServiceImp implements ExternalWeatherAPIService{
 
     @Override
