8c1f7ebb80ce1bf83299127d825a69f2cb73f9ca
Mark Ingamells <mark.ingamells@gmail.com>
2022-05-13 10:13:43 +0100

    GREEN Service Test Passing

--- src/test/java/com/techreturners/masterofarts/weatheradvisor/service/AdvisorServiceTest.java
+++ src/test/java/com/techreturners/masterofarts/weatheradvisor/service/AdvisorServiceTest.java
@@ -1,7 +1,41 @@
 package com.techreturners.masterofarts.weatheradvisor.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.techreturners.masterofarts.weatheradvisor.model.Weather;
+import com.techreturners.masterofarts.weatheradvisor.repository.ExternalWeatherAPIService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AdvisorServiceTest {
+
+    @Mock
+    private ExternalWeatherAPIService mockExternalWeatherAPIService;
+
+    @InjectMocks
+    private AdvisorService advisorService;
+
+    @Test
+    public void testGetWeather(){
+        double temp = 17.3;
+        long rain = 0L;
+        int cloud = 75;
+        Weather weather = Weather
+                .builder()
+                .cloud(cloud)
+                .temp(temp)
+                .rain(rain)
+                .build();
+        when(mockExternalWeatherAPIService.getWeather()).thenReturn(weather);
+
+        assertThat(advisorService.getWeather()).isEqualTo(weather);
+
+    }
 
 }
