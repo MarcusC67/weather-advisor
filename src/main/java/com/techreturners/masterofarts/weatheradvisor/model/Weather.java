c151a9c92c895cbdef79df5a179af8f1dcfd5899
Mark Ingamells <mark.ingamells@gmail.com>
2022-05-13 08:14:18 +0100

    First basic test

--- src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Weather.java
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Weather.java
@@ -1,9 +1,11 @@
 package com.techreturners.masterofarts.weatheradvisor.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Weather {
 
