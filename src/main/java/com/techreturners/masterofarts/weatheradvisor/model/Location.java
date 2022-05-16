15868cc529519a4cf39522a4ea0f91817e421f54
Matt DaSilva <matt_das@hotmail.co.uk>
2022-05-16 15:28:39 +0100

    Refactor weather object

--- src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Location.java
+++ src/main/java/com/techreturners/masterofarts/weatheradvisor/model/Location.java
@@ -1,13 +1,36 @@
 package com.techreturners.masterofarts.weatheradvisor.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import lombok.Data;
 
+@Data
 @Builder
+@Schema(description = "Encapsulation of a Location")
 public class Location {
 
+    /**
+     * Location name
+     */
+    @Schema(description = "Location name")
     String name;
+
+    /**
+     * Country code
+     */
+    @Schema(description = "Country code of location")
     String countryCode;
+
+    /**
+     * Latitude of location
+     */
+    @Schema(description = "Latitude of location")
     double lat;
+
+    /**
+     * Longitude of location
+     */
+    @Schema(description = "Longitude of location")
     double lon;
 
 }
